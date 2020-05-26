6137207fbd9b112463d5d68d5ce250829e01c9d8
RoboCopGay <vinicios.sousa909@gmail.com>
2020-05-26 18:46:24 -0300

    created the site

---
+++ qrpage/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template as render
+
+qrpage = Flask('qrpage Society')
+qrpage.config['SECRET_KEY'] = 'generic_secret_key'
+
+@qrpage.route('/criar_link', methods = ['POST', 'GET'])
+def create():
+    return render('create.html')
+
+@qrpage.route('/', methods = ['POST', 'GET'])
+def index():
+    data = request.args.to_dict()
+    data['description'] = data['description'] if 'description' in data else None
+    data['instagram'] = data['instagram'] if 'instagram' in data else None
+    data['whatsapp'] = data['whatsapp'] if 'whatsapp' in data else None
+    data['email'] = data['email'] if 'email' in data else None
+    data['name'] = data['name'] if 'name' in data else None
+
+    data['instagram_link'] = data['instagram_link'] if 'instagram_link' in data else None
+    data['whatsapp_link'] = data['whatsapp_link'] if 'whatsapp_link' in data else None
+    data['email_link'] = data['email_link'] if 'email_link' in data else None
+    data['name_link'] = data['name_link'] if 'name_link' in data else None
+
+    return render('index.html', data=data)
---
+++ run.py
@@ -0,0 +1,4 @@
+from qrpage import qrpage
+
+if __name__ == '__main__':
+    qrpage.run('localhost', port='5000', debug=True)
