8bca86498229800d7c578534887d3b72f29c9b85
RoboCopGay <vinicios.sousa909@gmail.com>
2020-06-11 23:57:36 -0300

    improving a better data get form

--- qrpage/__init__.py
+++ qrpage/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask, request, render_template as render
 qrpage = Flask('qrpage Society')
 qrpage.config['SECRET_KEY'] = 'generic_secret_key'
 
+forms = ('description', 'slogan', 'instagram', 'whatsapp', 'email', 'name',
+         'instagram_link', 'whatsapp_link', 'email_link', 'back', 'logo')
+
 @qrpage.route('/criar_link', methods = ['POST', 'GET'])
 def create():
     return render('create.html')
@@ -10,17 +13,9 @@ def create():
 @qrpage.route('/', methods = ['POST', 'GET'])
 def index():
     data = request.args.to_dict()
-    data['slogan'] = data['slogan'] if 'slogan' in data else None
-    data['description'] = data['description'] if 'description' in data else None
-    data['instagram'] = data['instagram'] if 'instagram' in data else None
-    data['whatsapp'] = data['whatsapp'] if 'whatsapp' in data else None
-    data['email'] = data['email'] if 'email' in data else None
-    data['name'] = data['name'] if 'name' in data else None
 
-    data['instagram_link'] = data['instagram_link'] if 'instagram_link' in data else None
-    data['whatsapp_link'] = data['whatsapp_link'] if 'whatsapp_link' in data else None
-    data['email_link'] = data['email_link'] if 'email_link' in data else None
-    data['name_link'] = data['name_link'] if 'name_link' in data else None
+    for form in forms:
+        data[form] = data[form] if form in data else None
 
     styles = data['styles'] = data['styles'] if 'styles' in data else '/static/style.css'
 
